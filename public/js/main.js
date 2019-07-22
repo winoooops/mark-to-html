bc3e4f781dcab3b2bb028fdd15572fb6a0b9a727
Winoooops <Winoooops@Wills-MacBook-Pro.local>
2019-07-22 14:16:35 +1000

    cause I am not using Electron.js now, vanilla JS does not have access the marked module in node...so use api instead...

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+const markdownArea = document.querySelector('#markdown')
+const htmlArea = document.querySelector('.html')
+
+const markdownToHtml = (markdown) => {
+  htmlArea.innerHTML = marked(markdown)
+}
+
+
+markdownArea.addEventListener('keyup', (e) => {
+  const content = e.target.value
+  markdownToHtml(content)
+})
