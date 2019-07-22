46eb2de0a7ac4a415741fc8b58c5842db85e6f64
Winoooops <Winoooops@Wills-MacBook-Pro.local>
2019-07-22 14:20:13 +1000

    added a sample markdown file ...

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,6 @@ const markdownToHtml = (markdown) => {
   htmlArea.innerHTML = marked(markdown)
 }
 
-
 markdownArea.addEventListener('keyup', (e) => {
   const content = e.target.value
   markdownToHtml(content)
