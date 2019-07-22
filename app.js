0cab1c0de4ed0513196048e2dbe65f8996f7e97e
Winoooops <Winoooops@Wills-MacBook-Pro.local>
2019-07-22 13:50:06 +1000

    adding views to the project, along with static assets...

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ const PORT = 3000;
 // bodyParse middleware that implements json type data
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+// 
+app.use(express.static('public'))
 
 // using handlebars as template
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Week One'
+  })
+})
 
 app.listen(PORT, () => {
   console.log("The serve is now online...")
